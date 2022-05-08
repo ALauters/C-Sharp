fd7592a15e5476711ce9169d540a84a325636641
alauters <austin-lauters@uiowa.edu>
2022-05-07 19:24:11 -0500

    clean up the repo a bit <3

---
+++ File_IO_Basics/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //C:\Users\Austin\Desktop\git\C-Sharp\File_IO_Basics\File_IO_Basics
+            string filepath = Path.Combine("C:\\","Users", "Austin", "Desktop", "git", "C-Sharp", "File_IO_Basics", "File_IO_Basics" )
+        }
+    }
+}
