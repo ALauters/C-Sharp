8169baef266a2f322d1d09624e5442d1cc103a7d
alauters <austin-lauters@uiowa.edu>
2022-05-07 19:56:02 -0500

    basic file io done

--- File_IO_Basics/Program.cs
+++ File_IO_Basics/Program.cs
@@ -6,8 +6,24 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            //C:\Users\Austin\Desktop\git\C-Sharp\File_IO_Basics\File_IO_Basics
-            string filepath = Path.Combine("C:\\","Users", "Austin", "Desktop", "git", "C-Sharp", "File_IO_Basics", "File_IO_Basics" )
+            string filepath = Path.Combine("C:\\", "Users", "Austin", "Desktop", "git", "C-Sharp", "File_IO_Basics", "test.txt");
+
+            List<string> lines = new List<string>();
+            lines = File.ReadAllLines(filepath).ToList();
+
+            // Console.WriteLine(filepath);
+
+            // Printing out all the lines from the file
+            foreach(String line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            lines.Add("Brian, Purple, 26");
+            File.WriteAllLines(filepath, lines);
+
+            
+
         }
     }
 }
